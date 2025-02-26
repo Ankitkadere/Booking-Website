a03ccb194a8687fa6a6038dc20da930a01146e00
Ankit Kadere <ankitkadere2904@gmail.com>
2025-02-26 13:32:37 +0530

    Add files via upload

--- Room Booking/Database.js
+++ Room Booking/Database.js
@@ -31,7 +31,7 @@ onAuthStateChanged(auth, (user) => {
   if (user) {
     loadUserData(user.uid);
   } else {
-    alert("No user is logged in.");
+    
     window.location.href = "Dahsboard.html"; // Redirect to login
   }
 });
@@ -84,7 +84,7 @@ async function loadUserData(userId) {
 document.getElementById("logout").addEventListener("click", () => {
   signOut(auth)
     .then(() => {
-      alert("Logged out successfully!");
+      
       window.location.href = "index.html"; // Redirect to login page
     })
     .catch((error) => {
